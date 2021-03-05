eb200f208893ff4e4f6fa03ce865981d1219e733
cgcostume <cgdaniel@outlook.com>
2021-03-06 00:06:22 +0100

    continue with the basics

--- index.js
+++ index.js
@@ -1,67 +1,172 @@
 
 class Renderer extends gloperate.Renderer {
 
-    constructor() {
-        super();
+    _camera;                // : gloperate.Camera
+    _navigation;            // : gloperate.Navigation
 
-        this._program = null;
+    _depthRenderbuffer;     // : gloperate.Renderbuffer
+    _colorRenderTexture;    // : gloperate.Texture2D
+    _intermediateFBO;       // : gloperate.Framebuffer
 
-        this._fontTexture = null;
+    _ndcFillingTriangle;    // : gloperate.NdcFillingTriangle
+    _ndcOffsetKernel;       // : gloperate.AntiAliasingKernel
+    // _uNdcOffset;            // : WebGLUniformLocation
 
-        this._targetTexture = null;
-        this._targetFBO = null;
+    _program;               // : gloperate.Program
+    // _uViewProjection;       // : WebGLUniformLocation
 
-        this._accumulate = null;
-    }
+    _defaultFBO;            // : gloperate.DefaultFramebuffer
 
-    onInitialize(context, callback, mouseEventProvider) {
+    _accumulate;            // : gloperate.AccumulatePass
+    _blit;                  // : gloperate.BlitPass
 
-        const gl = this._context.gl;
-
-
-        const internalFormatAndType = gloperate.Wizard.queryInternalTextureFormat(
-            this._context, gl.RGB, gloperate.Wizard.Precision.byte);
+    _labelPass;		        // : gloperate.LabelRenderPass;
+    _labels;		        // : Array of gloperate:Labels
 
 
-        this._targetTexture = new gloperate.Texture2D(this._context, 'TargetTexture');
-        this._targetTexture.initialize(1, 1, 
-            internalFormatAndType[0], gl.RGB, internalFormatAndType[1]);
-
-        this._targetFBO = new gloperate.Framebuffer(this._context, 'TargetFBO');
-        this._targetFBO.initialize([[this._context.gl2facade.COLOR_ATTACHMENT0, this._targetTexture] ]);      
-        this._targetFBO.bind();
+    onInitialize(context, callback, eventProvider) {
     
-        this._fontTexture = new gloperate.Texture2D(this._context);
-        this._fontTexture.initialize(1, 1, internalFormatAndType[0], gl.RGB, internalFormatAndType[1]);
+        const gl = context.gl;
+        const gl2facade = this._context.gl2facade;
+    
+        this._defaultFBO = new gloperate.DefaultFramebuffer(context, 'DefaultFBO');
+        this._defaultFBO.initialize();
+
+        const internalFormatAndType = gloperate.Wizard.queryInternalTextureFormat(this._context,
+            gl.RGBA, gloperate.Wizard.Precision.half);
+
+        this._colorRenderTexture = new gloperate.Texture2D(this._context, 'ColorRenderTexture');
+        this._colorRenderTexture.initialize(1, 1, internalFormatAndType[0], gl.RGBA, internalFormatAndType[1]);
+        this._colorRenderTexture.filter(gl.LINEAR, gl.LINEAR);
+
+        this._depthRenderbuffer = new gloperate.Renderbuffer(this._context, 'DepthRenderbuffer');
+        this._depthRenderbuffer.initialize(1, 1, gl.DEPTH_COMPONENT16);
+
+        this._intermediateFBO = new gloperate.Framebuffer(this._context, 'IntermediateFBO');
+        this._intermediateFBO.initialize([
+            [gl2facade.COLOR_ATTACHMENT0, this._colorRenderTexture],
+            [gl.DEPTH_ATTACHMENT, this._depthRenderbuffer]]);
+
+        // this._uViewProjection = this._program.uniform('u_viewProjection');
+        // this._uNdcOffset = this._program.uniform('u_ndcOffset');
 
 
-        const vert = new gloperate.Shader(this._context, gl.VERTEX_SHADER, 'debug (in-line)');
-        vert.initialize(this.SHADER_SOURCE_VERT);
+        // this._texture = new Texture2D(context, 'Texture');
+        // this._texture.initialize(1, 1, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE);
+        // this._texture.wrap(gl.REPEAT, gl.REPEAT);
+        // this._texture.filter(gl.LINEAR, gl.LINEAR_MIPMAP_LINEAR);
+        // this._texture.maxAnisotropy(Texture2D.MAX_ANISOTROPY);
 
-        const frag = new gloperate.Shader(this._context, gl.FRAGMENT_SHADER, 'debug (in-line)');
-        frag.initialize(this.SHADER_SOURCE_FRAG);
+        // this._texture.fetch('/examples/data/blue-painted-planks-diff-1k-modified.webp').then(() => {
+        //     const gl = context.gl;
 
-        this._program = new gloperate.Program(this._context, 'DebugProgram');
-        this._program.initialize([vert, frag], false);
+        //     this._program.bind();
+        //     gl.uniform1i(this._program.uniform('u_textured'), true);
+
+        //     this.finishLoading();
+        //     this.invalidate(true);
+        // });
+
+        this._camera = new gloperate.Camera();
+        this._camera.center = gloperate.vec3.fromValues(0.0, 0.0, 0.0);
+        this._camera.up = gloperate.vec3.fromValues(0.0, 1.0, 0.0);
+        this._camera.eye = gloperate.vec3.fromValues(0.0, 0.0, 5.0);
+        this._camera.near = 1.0;
+        this._camera.far = 8.0;
 
 
-        this._program.attribute('a_vertex', this._ndcRectangle.vertexLocation);
-        this._program.link();       
+        this._navigation = new gloperate.Navigation(callback, eventProvider);
+        this._navigation.camera = this._camera;
 
-        this._program.bind();
-        gl.uniform1i(this._program.uniform('u_font'), 0);
+        this._ndcTriangle = new gloperate.NdcFillingTriangle(context);
+        this._ndcTriangle.initialize(); 
 
-        this._uNdcOffset = this._program.uniform('u_ndcOffset');
+        this._accumulate = new gloperate.AccumulatePass(context);
+        this._accumulate.initialize(this._ndcTriangle);
+        this._accumulate.precision = this._framePrecision;
+        this._accumulate.texture = this._colorRenderTexture;
+        this._accumulate.update();
 
-        
-        this._accumulate = new gloperate.AccumulatePass(this._context);
-        this._accumulate.initialize();
-        this._accumulate.precision = gloperate.Wizard.Precision.byte;
+        this._blit = new gloperate.BlitPass(this._context);
+        this._blit.initialize(this._ndcTriangle);
+        this._blit.readBuffer = gl2facade.COLOR_ATTACHMENT0;
+        this._blit.target = this._defaultFBO;
+        this._blit.drawBuffer = gl.BACK;
+
+
+        // this._labelPass = new gloperate.LabelRenderPass(context);
+        // this._labelPass.initialize();
+        // this._labelPass.camera = this._camera;
+        // this._labelPass.target = this._defaultFBO;
+        // this._labelPass.depthMask = true;
+        // this._labelPass.aaStepScale = 0.5;
+        // this._labelPass.aaSampling = gloperate.LabelRenderPass.Sampling.Grid4x4;
+        // this._labelPass.depthFunc = context.gl.LESS;
+
+        // gloperate.FontFace.fromFile('./assets/roboto.fnt', context)
+        //     .then((fontFace) => {
+
+        //         // >> CHANGE LABELING TEXTS, POSITIONS, COLOR, AND MORE HERE:
+        //         // >> note: dynamic labels need to altered manually... (see on Frame)
+
+        //         var labels = new Array(3);
+
+        //         labels[0] = new gloperate.Position3DLabel(new gloperate.Text('Scatterplot'), gloperate.Label.Type.Static);
+        //         labels[0].lineAnchor = gloperate.Label.LineAnchor.Bottom;
+        //         labels[0].alignment = gloperate.Label.Alignment.Center;
+        //         labels[0].position = [0.0, 0.0, 0.0];
+        //         labels[0].direction = [1.0, 1.0, -1.0];
+        //         labels[0].up = [-1.5, 0.5, -1.0];
+        //         labels[0].fontSize = 0.3;
+        //         labels[0].fontSizeUnit = gloperate.Label.Unit.World;
+        //         labels[0].color.fromHex('#ffffff');
+
+        //         // labels[1] = new gloperate.Position3DLabel(new gloperate.Text('3D'), gloperate.Label.Type.Static);
+        //         // labels[1].lineAnchor = gloperate.Label.LineAnchor.Top;
+        //         // labels[1].alignment = gloperate.Label.Alignment.Center;
+        //         // labels[1].position = [-0.1, 0.2, 0.0];
+        //         // labels[1].direction = [1.0, 1.0, -1.0];
+        //         // labels[1].up = [-0.5, 1.5, +1.0];
+        //         // labels[1].fontSize = 1.2;
+        //         // labels[1].fontSizeUnit = gloperate.Label.Unit.World;
+        //         // labels[1].color.fromHex('#888888');
+
+        //         // labels[2] = new gloperate.Position3DLabel(new gloperate.Text('Probably the x-Axis'), gloperate.Label.Type.Static);
+        //         // labels[2].lineAnchor = gloperate.Label.LineAnchor.Bottom;
+        //         // labels[2].alignment = gloperate.Label.Alignment.Center;
+        //         // labels[2].position = [0.0, -1.1, -1.1];
+        //         // labels[2].up = [0.0, 0.0, -1.0];
+        //         // labels[2].direction = [1.0, 0.0, 0.0];
+        //         // labels[2].fontSize = 0.2;
+        //         // labels[2].fontSizeUnit = gloperate.Label.Unit.World;
+        //         // labels[2].color.fromHex('#ff00ff');
+
+        //         // labels[3] = new gloperate.Projected3DLabel(new gloperate.Text('   Point'), gloperate.Label.Type.Dynamic);
+        //         // labels[3].lineAnchor = gloperate.Label.LineAnchor.Bottom;
+        //         // labels[3].alignment = gloperate.Label.Alignment.Left;
+        //         // labels[3].position = [-1.0, +1.0, +1.0];
+        //         // labels[3].fontSize = 16.0;
+        //         // labels[3].fontSizeUnit = gloperate.Label.Unit.Mixed;
+        //         // labels[3].color.fromHex('#00ffff');
+
+        //         for (var label of labels) {
+        //             label.fontFace = fontFace;
+        //         }
+        //         this._labelPass.labels = labels;
+        //         this._labelPass.update();
+
+        //         this._labels = labels;
+
+        //         this.invalidate(true);
+        //     })
+        //     .catch((reason) => gloperate.auxiliaries.log(
+        //         gloperate.auxiliaries.LogLevel.Error, reason));
+
 
         return true;
     }
 
-    protected onUninitialize(): void {
+    onUninitialize() {
         super.uninitialize();
 
         this._cuboid.uninitialize();
@@ -69,6 +174,9 @@ class Renderer extends gloperate.Renderer {
 
         this._defaultFBO.uninitialize();
         this._accumulate.uninitialize();
+
+        
+        this._labelPass.uninitialize();
     }
 
 
@@ -82,13 +190,17 @@ class Renderer extends gloperate.Renderer {
     onUpdate() { 
         this._navigation.update();
 
-        return this._altered.any || this._camera.altered;
+        return this._altered.any || this._camera.altered;       
+        // return true; // continuous rendering
     }
 
     onPrepare() {
 
         if (this._altered.frameSize) {
             this._intermediateFBO.resize(this._frameSize[0], this._frameSize[1]);
+            this._camera.viewport = this._canvasSize;
+
+            this._camera.aspect = this._canvasSize[0] / this._canvasSize[1];
             this._camera.viewport = this._canvasSize;
         }
 
@@ -99,7 +211,7 @@ class Renderer extends gloperate.Renderer {
         }
 
         if (this._altered.multiFrameNumber) {
-            this._ndcOffsetKernel = new AntiAliasingKernel(this._multiFrameNumber);
+            this._ndcOffsetKernel = new gloperate.AntiAliasingKernel(this._multiFrameNumber);
         }
 
         this._accumulate.update();
@@ -108,7 +220,7 @@ class Renderer extends gloperate.Renderer {
         this._camera.altered = false;
     }
 
-    onFrame() {
+    onFrame(frameNumber) {
         const gl = this._context.gl;
 
         this._intermediateFBO.bind();
@@ -124,24 +236,20 @@ class Renderer extends gloperate.Renderer {
 
         gl.enable(gl.DEPTH_TEST);
 
-        
-        this._fontTexture.bind(gl.TEXTURE0);
-
-        this._targetFBO.bind();
-        this._program.bind();
+        // this._program.bind();
 
 
-        gl.uniform2fv(this._uNdcOffset, ndcOffset);
+        // gl.uniform2fv(this._uNdcOffset, ndcOffset);
 
 
         // this._ndcRectangle.bind();
         // this._ndcRectangle.draw();
         // this._ndcRectangle.unbind();
 
-        this._program.unbind();
-        this._targetFBO.unbind();
+        // this._program.unbind();
+        this._intermediateFBO.unbind();
         
-        this._texture.unbind(gl.TEXTURE0);
+        // this._texture.unbind(gl.TEXTURE0);
 
 
         this._accumulate.frame(frameNumber);
@@ -165,72 +273,70 @@ class Renderer extends gloperate.Renderer {
 
     input(file, callback) {
 
-        this._debug = true;
+        // var fr = new FileReader();
+        // fr.onload = () => {
+        //     // this._fontTexture.load(fr.result).then(() => {
+        //     //     const gl = this._context.gl;
 
-        var fr = new FileReader();
-        fr.onload = () => {
-            // this._fontTexture.load(fr.result).then(() => {
-            //     const gl = this._context.gl;
+        //     //     this._fontTexture.bind(gl.TEXTURE0);
+        //     //     this._fontTexture.wrap(gl.REPEAT, gl.REPEAT, false, false);
+        //     //     this._fontTexture.filter(gl.LINEAR, gl.LINEAR, false, false); 
 
-            //     this._fontTexture.bind(gl.TEXTURE0);
-            //     this._fontTexture.wrap(gl.REPEAT, gl.REPEAT, false, false);
-            //     this._fontTexture.filter(gl.LINEAR, gl.LINEAR, false, false); 
+        //     //     this._debug = false;
 
-            //     this._debug = false;
-
-            //     callback();
-            // });
-        };
-        fr.readAsDataURL(file);
+        //     //     callback();
+        //     // });
+        // };
+        // fr.readAsDataURL(file);
     }
 
 }
 
 
-Renderer.prototype.SHADER_SOURCE_VERT = `
-precision highp float;
-precision highp int;
+// Renderer.prototype.SHADER_SOURCE_VERT = `
+// precision highp float;
+// precision highp int;
 
-#if __VERSION__ == 100
-    attribute vec2 a_vertex;
-#else
-    in vec2 a_vertex;
-    #define varying out
-#endif
+// #if __VERSION__ == 100
+//     attribute vec2 a_vertex;
+// #else
+//     in vec2 a_vertex;
+//     #define varying out
+// #endif
 
-uniform vec2 u_ndcOffset;
+// uniform vec2 u_ndcOffset;
 
-varying vec2 v_uv;
+// varying vec2 v_uv;
 
-void main(void)
-{
-    vec2 v = a_vertex;
-    v_uv = a_vertex;
+// void main(void)
+// {
+//     vec2 v = a_vertex;
+//     v_uv = a_vertex;
     
-    gl_Position = vec4(a_vertex.xy + u_ndcOffset, 0.0, 1.0);
-}
-`;
+//     gl_Position = vec4(a_vertex.xy + u_ndcOffset, 0.0, 1.0);
+// }
+// `;
 
 
-Renderer.prototype.SHADER_SOURCE_FRAG = `
-precision highp float;
-precision highp int;
+// Renderer.prototype.SHADER_SOURCE_FRAG = `
+// precision highp float;
+// precision highp int;
 
-#if __VERSION__ == 100
-    #define fragColor gl_FragColor
-    #define texture texture2D
-#else
-    layout(location = 0) out vec4 fragColor;
-    #define varying in
-#endif
+// #if __VERSION__ == 100
+//     #define fragColor gl_FragColor
+//     #define texture texture2D
+// #else
+//     layout(location = 0) out vec4 fragColor;
+//     #define varying in
+// #endif
 
-uniform sampler2D u_font;
+// uniform sampler2D u_font;
 
-varying vec2 v_uv;
+// varying vec2 v_uv;
 
-void main(void)
-{
-    fragColor = texture(u_font, v_uv);
-}
-`;
+// void main(void)
+// {
+//     fragColor = texture(u_font, v_uv);
+// }
+// `;
 
